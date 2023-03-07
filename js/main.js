0403bdd1c589771a7f4fd30885e602632c859f7f
AnthonyScott <anthony.scott1005@gmail.com>
2023-03-07 14:40:38 -0500

    Dynamically render board based on user input

--- js/main.js
+++ js/main.js
@@ -1,21 +1,62 @@
 /* Cached Elements */
 let boardSizeInp = document.getElementById("input");
-
+let mainEl = document.querySelector("main");
 
 /* Global Variables */
 let boardSize;
 
+/*Class Declarations*/
+class Board {
+    constructor(input){
+        this.rows = input;
+        this.cols = input;
+    }
+
+    generateBoard = () => {
+        //generating an empty 2D array
+        let boardArr = [];
+        for (let i =0;i<this.cols;i++){
+            boardArr[i] = [];
+        }
+        
+        //Dynamically creating a table HTML element
+        let table = document.createElement('table');
+        for (let i=0;i<this.rows;i++){
+            let eachRow = document.createElement('tr');
+            for (let j=0;j<this.cols;j++){
+                let eachCell = document.createElement('td');
+                eachRow.append(eachCell);
+            }
+            table.appendChild(eachRow);
+        }
+        mainEl.append(table);
+        console.log(mainEl);
+
+    }
+}
+
 /* Event Listeners */
+//submit button functionality
 boardSizeInp.addEventListener("submit", (e) =>{
+
     e.preventDefault();
 
     boardSize = document.getElementById("board-size");
-    boardSizeInp.style.display = "none";
+
+    //checking for valid inputs
+    if(boardSize.value > 20 || boardSize.value < 3){
+
+        alert("Invalid board size selected - Try Again");
+    } else {
+        //hides the input form and begins the game
+        boardSizeInp.style.display = "none";
+        init();
+    }
     
-    init();
 })
 
 /* Global Functions */
 const init = () =>{
-    console.log(boardSize.value);
+    gameOn = new Board(boardSize.value);
+    gameOn.generateBoard();
 }
--- planning.md
+++ planning.md
@@ -3,7 +3,7 @@
 
 ## Day 1
 Let's build and style the html layout then construct the board class and if we have time the factory method for instantiating the square/cell class
->HTML layout/submit button
+>HTML layout/submit button implementation and JS testing
 >Styling the layout
 >Board class
 >Factory method for cell/square instances
